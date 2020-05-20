ed70026877ac53083bd380cec8680c961ca15adf
igor.melo <igorjmelo4@gmail.com>
2020-05-20 00:18:40 -0300

    ADD: mapbox-gl lib + initial structure

---
+++ src/modules/main/MainPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './MainPage.module.css';
+
+import Mapbox from './components/Map/Mapbox';
+
+export default function MainPage() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.mapContainer}>
+        <Mapbox />
+      </div>
+      <div className={styles.chartContainer}>Gráfico</div>
+      <div className={styles.playerContainer}>Player</div>
+      <div className={styles.rankingContainer}>Ranking</div>
+    </div>
+  );
+}
